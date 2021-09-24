142120fdfdac070090f9bc0d2d2c518a9fe9ad4c
ahmedmagdy491 <ahmedahmedmagdy491@gmail.com>
2021-09-24 17:09:26 +0200

    done

---
+++ public/precache-manifest.a027083575d62a6447dc78de96f3d6f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a00650697eede35a4d7a3a6661b397f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e0a95d94d3afba5e47",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "890b9bbd17f9099bf565",
+    "url": "/static/js/2.b5295c05.chunk.js"
+  },
+  {
+    "revision": "a94751185011317bdf3fabb99649b8a5",
+    "url": "/static/js/2.b5295c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e0a95d94d3afba5e47",
+    "url": "/static/js/main.39f05f21.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
